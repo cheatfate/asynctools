ef042fed3c73e46fcb0cd35fd4e3fd1914671a32
cheatfate <ka@hardcore.kiev.ua>
2016-10-25 01:31:43 +0300

    Add asyncpty.nim to asynctools.nim

--- asynctools.nim
+++ asynctools.nim
@@ -1,3 +1,3 @@
 import asynctools/asyncpipe, asynctools/asyncipc, asynctools/asyncdns
-import asynctools/asyncproc
-export asyncpipe, asyncipc, asyncdns, asyncproc
+import asynctools/asyncproc, asynctools/asyncpty
+export asyncpipe, asyncipc, asyncdns, asyncproc, asyncpty
